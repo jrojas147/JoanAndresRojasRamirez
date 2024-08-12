1ad39df611abf5997f13fcb145d7dcebb990c787
Joan Andres Rojas Ramirez <jarojasramirez147@gmail.com>
2024-08-12 14:43:43 -0500

    documentacion  registryComponent

--- frontend/src/app/screen/registry/registry.component.ts
+++ frontend/src/app/screen/registry/registry.component.ts
@@ -15,6 +15,10 @@ import {
 import { ToastService } from '@components/toast/toast.service';
 import { ToastType } from '@components/toast/toast.model';
 
+
+/**
+ * Permite registrar o editar informacion de productos.
+ */
 @Component({
   selector: 'app-registry',
   templateUrl: './registry.component.html',
@@ -41,31 +45,26 @@ export class PageRegistryComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    console.log('Entra');
-    
     this.formAccounts = this.fb.group(this.createControls());
-
     this.formAccounts.controls['id'].valueChanges.subscribe(
       (selectedValue: string) => {
-        console.log(selectedValue);
-        console.log(this.formAccounts);
         this.checkIdAccount(selectedValue);
       }
     );
-
     if (this.idAccount) {
       this.bankService
         .getAccountsById(this.idAccount)
         ?.subscribe((response: any) => {
           this.account = response;
-
           this.setControlValues(this.account);
         });
     }
-
-    console.log(this.formAccounts);
   }
 
+  /**
+   * Perimite creacion de formulario
+   * @returns valor formulario
+   */
   public createControls() {
     const groupControl: any = {};
 
@@ -87,25 +86,36 @@ export class PageRegistryComponent implements OnInit {
       },
       Validators.required,
     ];
-
     return groupControl;
   }
 
+  /**
+   * permite asignar valores a formulario
+   * @param account valores de formulario a editar
+   */
   setControlValues(account: any) {
     Object.keys(this.formAccounts?.controls).forEach((key) => {
       this.formAccounts?.controls[key].setValue(account[key]);
     });
   }
 
-  checkIdAccount(idAccount: string) {
+  /**
+   * Permite realizar validaicon si existe producto mediante peticion http
+   * @param idAccount idProducto
+   */
+  public checkIdAccount(idAccount: string):void {
     this.bankService.verifyAccount(idAccount)?.subscribe((response: boolean) => {
       this.existAccount = response;
     });
   }
 
-  saveRegistry() {
+  /**
+   * Permite guardar la informacion de producto, desde la carga de componente se identifica tenemos un param id
+   * de ser asi se considera como una edicion, de caso contrario seria adicionar nuevo producto
+   * para cada caso de tiene un itervalo de tiempo para visualizar confirmacion de consumo de servicio
+   */
+  public saveRegistry(): void {
     const dataForm = this.formAccounts.getRawValue();
-
     if (this.idAccount) {
       this.bankService.putAccounts(dataForm)?.subscribe((response: AccountsListModel) => {
         this.listAccounts = response.data;
@@ -129,7 +139,10 @@ export class PageRegistryComponent implements OnInit {
     }
   }
 
-  clearValues() {
+  /**
+   * Permite limipar valores del formulario
+   */
+  public clearValues(): void {
     Object.keys(this.formAccounts?.controls).forEach((key) => {
       if (!(key === 'id' || key === 'date_revision')) {
         this.formAccounts?.controls[key].reset();
@@ -137,7 +150,10 @@ export class PageRegistryComponent implements OnInit {
     });
   }
 
-  backUrl() {
+  /**
+   * Permite retornar url prinicipal, pantalla de productos activos
+   */
+  public backUrl():void {
     this.router.navigateByUrl('/');
   }
 }
